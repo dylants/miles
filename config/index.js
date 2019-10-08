8f52833c7504cca35e8c556c70e6c5c7f1364037
Dylan Smith <dylantsmith@gmail.com>
2019-10-08 10:28:56 -0500

    Update config
    - Remove unused configuration details

--- config/index.js
+++ config/index.js
@@ -1,7 +1,1 @@
-const isDevelopment = process.env.NODE_ENV === 'development';
-
-const LOG_LEVEL = isDevelopment ? 'debug' : 'verbose';
-
-module.exports = {
-  logLevel: LOG_LEVEL, // [error, warn, info, verbose, debug, silly]
-};
+module.exports = {};
